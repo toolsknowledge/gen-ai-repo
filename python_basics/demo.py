f76c0db9073019bec52985e8d41ec0b7b0e14ee8
Samba <samba.pendela@gmail.com>
2026-05-15 19:56:45 +0530

    code

---
+++ python_basics/demo.py
@@ -0,0 +1,21 @@
+print("welcome to python !!!")
+
+num1 = 200
+num2 = 100
+addition = num1 + num2
+print(addition)
+print(f"Addition : {addition}")
+
+subtraction = num1 - num2
+print(f"Subtraction : {subtraction}")
+
+str1 = "Hello"
+str2 = 'Welcome'
+str3 = """
+    welcome to demo practice on python code
+    soon, we will start variables in python
+    later, we will discuss gen ai and agentic ai
+"""
+print(str1)
+print(str2)
+print(str3)
