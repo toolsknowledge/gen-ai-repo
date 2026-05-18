23a98e72020b0cbf4564b610c914285c55c43ac4
Samba <samba.pendela@gmail.com>
2026-05-18 22:03:41 +0530

    code

--- Python_Basics/pandas_ex.py
+++ Python_Basics/pandas_ex.py
@@ -29,7 +29,7 @@ import pandas as pd
 # print(df)
 
 # Example-6
-df = pd.read_csv("employees.csv")
+# df = pd.read_csv("employees.csv")
 # print(df)
 # print(df.head()) # read 5 rows
 # print(df.head(3)) # read 3 rows
@@ -55,3 +55,59 @@ df = pd.read_csv("employees.csv")
 
 # print(df.sort_values("Salary",ascending=False).head(1)[["Name","Department","Designation"]]) #  display highest paid employee
 # print(df[(df["Salary"]>70000) & (df["Department"]=="IT")]) # apply multiple conditions
+
+df = pd.read_csv("employees_null.csv")
+print(df)
+# nulls replaced with True
+# print(df.isnull())
+
+# nulls replaced with False
+# print(df.notnull())
+
+# nulls age column
+# print(df["age"].isnull())
+
+# nulls in salary column
+# print(df["salary"].isnull())
+
+# nulls in column wise
+# print(df.isnull().sum())
+
+# column wise nulls percentage 
+# print( df.isnull().sum() / len(df) * 100 )
+
+# replace missed values with 0
+# print(df.fillna(0))
+
+# implement forward fill
+# print(df.ffill())
+
+# implement the backward fill
+# print(df.bfill())
+
+# fill missed age with average age
+# df["age"] = df["age"].fillna(df["age"].mean())
+
+# fill missed salary with average salary
+# df["salary"] = df["salary"].fillna(df["salary"].mean())
+# print(df)
+
+# handle multiple missed values 
+# df.fillna({
+#     "age":df["age"].mean(),
+#     "salary":df["salary"].mean()
+# },inplace=True)
+# print(df)
+
+print("---------------------")
+# if any column missed then delete entire row
+# print(df.dropna())
+
+# if all columns are null, then only delete the row
+# print(df.dropna(how="all"))
+
+# if age & salary are null, then only drop the row
+# print(df.dropna(subset=["age","salary"]))
+
+# if row contains two non nulls,it wont delete
+# print(df.dropna(thresh=2))
