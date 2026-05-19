5e1dbf2e88ad3e41b3078dae8507a5f2a74fb460
Samba <samba.pendela@gmail.com>
2026-05-19 07:48:49 +0530

    env ready

---
+++ Python_Basics/demo.py
@@ -0,0 +1,15 @@
+print("welcome to python !!!")
+str = """
+    We will cover
+        1) Python with libraries
+        2) ML (Machine Learning)
+        3) DL (Deep Learning)
+        4) NLP (Natural Language Processing)
+        5) Gen AI (Generative AI)
+        6) Agentic AI
+        7) MLOps
+        8) LLMOps
+        9) Agent Deployment
+        10) RAG Protocol / MCP Client
+"""
+print(str)
