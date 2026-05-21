8509ad050782b08147e040db2bbbd5f15a967f04
Samba <samba.pendela@gmail.com>
2026-05-21 08:00:04 +0530

    code

---
+++ Python_Basics/test_ex.py
@@ -0,0 +1,6 @@
+from rich import print
+
+# print() is the predefined function, used to display data on console/terminal
+print("Hello, welcome to Python !!!")
+
+print("[bold red]Hello[/bold red]")
