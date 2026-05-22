5d9503fbf608f8a4c8ac52ec02c8a4402fae9263
Samba <samba.pendela@gmail.com>
2026-05-22 07:51:14 +0530

    code

---
+++ Python_Basics/test.py
@@ -0,0 +1,65 @@
+# t1 = (10,20,30) # tuple # immutable
+# print(t1[-3])
+# for element in t1:
+#     print(element)
+
+
+
+
+# arr1 = [10,20,30,40,50]
+# print(arr1[0], arr1[-5])
+# print(arr1[2], arr1[-3])
+# print(arr1[0:3]) # index "0" includes and index "3" will exclude
+# print(arr1[:2])  # index "0" includes and index "2" will exclude
+# print(arr1[2:])  # from "2" index to last index
+# print(arr1[::-1]) # reverse list
+# arr1[0] = 1000 # mutable
+# print(arr1)
+
+
+# True - 1. False - 0
+# print(True + True)
+# print(1 + True + 1)
+# print(True / False)
+# print(False / True)
+
+
+# flag = True
+# if flag:
+#     print("Agents")
+# else:
+#     print("Gen AI")
+
+
+
+# script = """
+#         1) Python (Numpy,Pandas, Matplotlib,...)
+#         2) Prompt Engg
+#         3) RAG
+#         4) MCP Server with MCP Client
+#         5) Agent Development
+#         6) Multo Agent Communication
+#         7) Deploy (AWS / Azure / GCP)
+# """
+# print("Read Map for Working Professionals: {}".format(script))
+
+
+
+# sub = "Agentic AI"
+# print("Welcome to {}".format(sub))
+
+
+# num1 = 0x16         # 22
+# num2 = 0o123        # 83
+# subtraction = num2 - num1
+# print(f"Subtraction :{subtraction}")
+
+
+# num1 = 200
+# num2 = 100
+# res = num1 + num2
+# print(f"ABC : {res}")
+
+
+
+
